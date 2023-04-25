d1ef4dc5a5c49f6e1afb3bd7b341330883e36ab3
Abdikarim Issa <kareemorbit@gmail.com>
2023-04-25 19:31:41 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>TS Amazona</header>
+      <h1>Home Page</h1>
+      <footer>All rights reserved</footer>
+    </main>
+  );
+}
+
+export default App;
