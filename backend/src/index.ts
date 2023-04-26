47fac3186f433a1017070ec5ba028e62ccf8ef7b
Abdikarim Issa <kareemorbit@gmail.com>
2023-04-26 08:55:23 +0300

    connect frontend to backend and fetch products

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,20 @@
 import express, { Request, Response } from "express";
 import { sampleProducts } from "./data";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
 app.get("/api/products", (req: Request, res: Response) => {
   res.json(sampleProducts);
 });
-const PORT = process.env.PORT || 4080;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
