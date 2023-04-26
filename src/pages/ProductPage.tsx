f781402d378114bdf6c5452ae08fb001c9cdee85
Abdikarim Issa <kareemorbit@gmail.com>
2023-04-26 20:04:03 +0300

    adding product details page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,12 +1,91 @@
 import { Helmet } from "react-helmet-async";
+import { useGetProductDetails } from "../hooks/productHooks";
+import { useParams } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
+import { ApiError } from "../types/ApiError";
+import { Badge, Button, Card, Col, ListGroup, Row } from "react-bootstrap";
+import Rating from "../components/Rating";
 
 export default function ProductPage() {
-  return (
+  const { slug } = useParams();
+  const {
+    isLoading,
+    isError,
+    error,
+    data: product,
+  } = useGetProductDetails(slug!);
+
+  return isLoading ? (
+    <LoadingBox />
+  ) : isError ? (
+    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
+  ) : (
     <div>
+      ()
       <Helmet>
         <title>Product Page</title>
       </Helmet>
-      <h1>Product Page</h1>
+      <Row>
+        <Col md={6}>
+          <img className="large" src={product.image} alt={product.name}></img>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Helmet>
+                <title>{product.name}</title>
+              </Helmet>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                rating={product.rating}
+                numReviews={product.numReviews}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              Description:
+              <p>{product.description}</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>${product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? (
+                        <Badge bg="success">In Stock</Badge>
+                      ) : (
+                        <Badge bg="danger">Unavailable</Badge>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <div className="d-grid">
+                      <Button variant="primary">Add to Cart</Button>
+                    </div>
+                  </ListGroup.Item>
+                )}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
